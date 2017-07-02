01395d3d5bb31e2bf9776bc0ddfe33ef727be8a5
MangoMM <ahmet@mangomm.co.uk>
2017-07-02 08:59:46 +0100

    Basic API

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"log"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	ADDR = ":8000"
+)
+
+type product struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Price string `json:"price"`
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/products", productsHandler)
+
+	http.Handle("/", r)
+
+	log.Println("API Server Started on", ADDR)
+	http.ListenAndServe(ADDR, nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(200)
+
+	body := "Hello from home"
+
+	json.NewEncoder(w).Encode(body)
+}
+
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(200)
+
+	products := []product{
+		{1, "XBOX", "200.02"},
+		{2, "Playstation", "300.03"},
+		{3, "Wii", "100.01"},
+	}
+
+	json.NewEncoder(w).Encode(products)
+}
